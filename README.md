05b6a8570679156513691c05319178f0a3a6b889
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-03 00:20:41 +0100

    model success and db connected

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # YelpCamp
 Full Basic Express App
+
+A Camp Ground with a name, Price, Description AND Location
+
 Developer: Tolulope Fakunle
--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    autoIndex: true,
+    useUnifiedTopology: true
+}) //creates a database Yelp-Camp with some options 
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+}); // logic to check if there is an error & successfully opened
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
-
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({
+        title: 'My Backyard',
+        description: 'Cheap Campground around you!',
+        location: 'Edo State'
+    });
+    await camp.save();
+    res.send(camp);
 })
 
 
