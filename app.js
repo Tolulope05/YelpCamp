9501f7233cda28ce696b0f338934bcec49f3df9c
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-03 21:24:58 +0100

    delete campground success

--- app.js
+++ app.js
@@ -60,6 +60,12 @@ app.put('/campgrounds/:id', async (req, res) => {
 
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
