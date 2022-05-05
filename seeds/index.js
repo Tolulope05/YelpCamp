f3eddb3faa10e5a663355fbb888f277334df998b
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-05 13:21:34 +0100

    Adding image and description to our model and seedingour db

--- seeds/index.js
+++ seeds/index.js
@@ -21,10 +21,14 @@ const seedDB = async () => {
     await Campground.deleteMany({}); // To Clear our Database
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground(
             {
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
-                title: `${sample(descriptors)}, ${sample(places)}`
+                title: `${sample(descriptors)}, ${sample(places)}`,
+                image: 'https://picsum.photos/300/200',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut debitis ipsam eum repudiandae laboriosam! Impedit nostrum eius possimus doloremque asperiores iusto recusandae dolor quam, quaerat dignissimos eos alias ut maiores',
+                price
             }
         )
         await camp.save();
