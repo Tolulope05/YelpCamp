f54be907f38c38816df483dfeea8cb53ee956046
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-08 02:01:34 +0100

    successfully created reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utilities/catchAsync') //Asynchronous Error Handle
 const ExpressError = require('./utilities/ExpressError') //Express Error Class
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -82,6 +83,16 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
+}))
+
+/**CREATING REVIEWS */
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 /** ASYNC ERROR HANDLING */
