60e17fcdd75a2c31211126223cdf51f14ab8cc8a
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-10 22:57:28 +0100

    basic registeration with passport

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utilities/catchAsync')
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    // const newUser = await new User({ name: req.body.name, email: req.body.email });
-    res.send(req.body);
-});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = await new User({ username, email });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to YELP CAMP!');
+        res.redirect('/campgrounds');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
