3ac0e11d6a9db4f12100a34c0d74962e1168c461
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-11 14:10:07 +0100

    review permission success

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utilities/catchAsync') //Asynchronous Error Handler 
 const ExpressError = require('../utilities/ExpressError') //Express Error Class
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
 /**CREATING REVIEWS */
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id; // To add the user objectId
     campground.reviews.push(review);
     await review.save();
     await campground.save();
