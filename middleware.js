f59d43e1bd0a1d849d59ec091a65288eb3a5780a
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-11 10:45:37 +0100

    basic functionality return to behaviour success

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log('REQ.USER...', req.user);
+    // console.log('REQ.USER...', req.user);
+    // Store the url they are requesting!
+    req.session.returnTo = req.originalUrl;
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be signed in!');
         return res.redirect('/login');
