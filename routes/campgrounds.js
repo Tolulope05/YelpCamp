faa30c53dfe994dc07dfb912f857ad11bafe6fae
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-12 15:01:23 +0100

    uploading to cloudinary success

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,12 +4,16 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utilities/catchAsync') //Asynchronous Error Handler 
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); //Middleware
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary')
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.array('image'), (req, res) => { console.log(req.body, req.files); res.send('Uploaded'); });
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('Uploaded');
+    });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm); //new form
 
