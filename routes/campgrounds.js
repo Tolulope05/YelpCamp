1a1ea277500ab9ccaa54d3976416ff044cc71e44
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-12 12:32:09 +0100

    multer middleware success

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utilities/catchAsync') //Asynchronous Error Handler 
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); //Middleware
-const Campground = require('../models/campground');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => { console.log(req.body, req.files); res.send('Uploaded'); });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm); //new form
 
