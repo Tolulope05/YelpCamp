090deb38455f04de66f7e329b305b7e4708fdf24
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-13 12:45:40 +0100

    geocoding our location

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -11,14 +14,20 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground);
-    req.flash('success', 'Successfully made a new Campground!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    res.send(geoData.body.features[0].geometry.coordinates);
+    // res.send('ok')
+    // const campground = new Campground(req.body.campground);
+    // campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
+    // campground.author = req.user._id;
+    // await campground.save();
+    // console.log(campground);
+    // req.flash('success', 'Successfully made a new Campground!')
+    // res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.showCampground = async (req, res) => {
     const { id } = req.params;
@@ -33,7 +42,7 @@ module.exports.showCampground = async (req, res) => {
         return res.redirect('/campgrounds')
     }
     res.render('campgrounds/show', { campground });
-}
+};
 
 module.exports.renderEditform = async (req, res) => {
     const { id } = req.params;
