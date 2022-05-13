329555b8a1b52ebe6b07034e10ead2e5ff7ece90
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-13 06:45:02 +0100

    solved error of unability yo update

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -46,10 +46,15 @@ module.exports.renderEditform = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body)
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(file => ({ url: file.path, filename: file.filename }));
     campground.images.push(...imgs);
-    await campground.save()
+    if (req.body.deleteImages) {
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+        console.log(campground)
+    }
+    await campground.save();
     req.flash('success', 'Successfully updated Campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }
