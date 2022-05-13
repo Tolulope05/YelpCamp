ff481e6c8825ccab39c9e903bb96edc377accf4f
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-13 21:43:46 +0100

    Adding earthquake cluster map success

--- public/javascripts/showpageMap.js
+++ public/javascripts/showpageMap.js
@@ -3,13 +3,19 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: parsedCamp.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 10 // starting zoom
 });
 
 // Set marker options.
 const marker = new mapboxgl.Marker({
     color: "#FF0000",
-    draggable: true
+    draggable: false,
 }).setLngLat(parsedCamp.geometry.coordinates)
     .addTo(map)
-    .marker();
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${parsedCamp.title}</h3> <p>${parsedCamp.description}</p>`
+            )
+    )
+    .marker()
