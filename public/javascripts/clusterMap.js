eae82b99be7b4812eb932045280778707ecce9f9
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-14 17:35:24 +0100

    home styling success

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.5917, 40.6699],
     zoom: 3
 });
